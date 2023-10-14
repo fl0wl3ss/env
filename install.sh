8eeee6149a2e92a53ea168cd8952a982c8d6e51c
root <root@LAPTOP-AJ7N12JV.>
2023-10-14 19:05:36 +0900

    update

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # install tmux-powerline
-git clone https://github.com/fl0wl3ss/tmux-powerline.git ~/Github
+git clone https://github.com/fl0wl3ss/tmux-powerline.git ~/Github/tmux-powerline
 
 # copy configs
 mkdir -p ~/bin
